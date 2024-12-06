baae1a4147e4e1c1b3171d99c96ef83f43c4d653
Archit Kakkar <education.archit@gmail.com>
2024-12-06 16:17:41 +0530

    exercise 1.4 completed.

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,16 +1,18 @@
 import PropTypes from "prop-types";
 
 const Content = (props) => {
+  
   return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
+    <>
+      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
+      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
+    </>
   );
 };
 
 Content.propTypes = {
-  part: PropTypes.string,
-  exercises: PropTypes.number,
+  parts: PropTypes.array,
 };
 
 export default Content;
--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -2,14 +2,12 @@ import PropTypes from "prop-types";
 
 const Total = (props) => {
     return (
-        <p>Number of Exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+        <p>Number of Exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     );
 }
 
 Total.propTypes = {
-    exercises2: PropTypes.number,
-    exercises3: PropTypes.number,
-    exercises1: PropTypes.number,
+    parts: PropTypes.array,
 }
 
 export default Total;
