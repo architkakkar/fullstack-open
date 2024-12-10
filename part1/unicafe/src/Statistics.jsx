6935c52cef095d2988aab5cf014656d67303e30e
Archit Kakkar <education.archit@gmail.com>
2024-12-11 02:15:12 +0530

    exercise 1.9: conditional render statistics.

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,26 +1,35 @@
 import PropTypes from "prop-types";
 
 const Statistics = (props) => {
-  return (
-    <div>
-      <h1>statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.total}</p>
-      <p>average {props.average}</p>
-      <p>positive {props.positive} %</p>
-    </div>
-  );
+  if (props.total === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good {props.good}</p>
+        <p>neutral {props.neutral}</p>
+        <p>bad {props.bad}</p>
+        <p>all {props.total}</p>
+        <p>average {props.average}</p>
+        <p>positive {props.positive} %</p>
+      </div>
+    );
+  }
 };
 
 Statistics.propTypes = {
-    good: PropTypes.number,
-    neutral: PropTypes.number,
-    bad: PropTypes.number,
-    total: PropTypes.number,
-    average: PropTypes.number,
-    positive: PropTypes.number,
-}
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+  average: PropTypes.number,
+  positive: PropTypes.number,
+};
 
 export default Statistics;
