d23eedd89ebe5316c43d1a032745a2fa7291a2af
Archit Kakkar <education.archit@gmail.com>
2024-12-11 00:38:17 +0530

    MOOC part1d completed.

--- playground/src/App.jsx
+++ playground/src/App.jsx
@@ -1,32 +1,67 @@
+import PropTypes from 'prop-types'
 import { useState } from 'react'
-import Display from './Display'
-import Button from './Button'
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const increase = () => setCounter(counter + 1)
-  const decrease = () => setCounter(counter - 1)
-  const reset = () => setCounter(0)
-
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      <Display counter={counter}/>
-
-      <Button
-        onClick={increase}
-        text='plus'
-      />
-      <Button
-        onClick={reset}
-        text='zero'
-      />     
-      <Button
-        onClick={decrease}
-        text='minus'
-      />           
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
-export default App
+History.propTypes = {
+  allClicks: PropTypes.array.isRequired,
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+Button.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const newLeft = left + 1;
+    setLeft(newLeft)
+    setTotal(newLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const newRight = right + 1;
+    setRight(newRight)
+    setTotal(left + newRight)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+      <p>{total}</p>
+    </div>
+  )
+}
+
+export default App;
