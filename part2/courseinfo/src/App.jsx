43fa40e0c7739dc3fa6c2d0268b2845ab667d025
Archit Kakkar <education.archit@gmail.com>
2024-12-17 00:03:47 +0530

    exercise 2.4: handling arbitrary number of courses.

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-const Header = ({ name }) => <h1>{name}</h1>;
+const Header = ({ name }) => <h2>{name}</h2>;
 
 Header.propTypes = { name: PropTypes.string };
 
@@ -45,39 +45,59 @@ const Course = ({ course }) => {
 Course.propTypes = { course: PropTypes.object };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Router",
-        exercises: 20,
-        id: 4,
-      },
-      {
-        name: "Redux",
-        exercises: 5,
-        id: 5,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
+      ))}
+    </div>
+   );
 };
 
 export default App;
