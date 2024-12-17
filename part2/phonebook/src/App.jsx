61eb4aee5bf3af8dfbf7630857c394701a3024a8
Archit Kakkar <education.archit@gmail.com>
2024-12-17 18:33:47 +0530

    exercise 2.8: add phone numbers to the phone book.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const addName = (e) => {
+  const addContact = (e) => {
     e.preventDefault();
 
     const trimmedName = newName.trim();
 
     const newPerson = {
       name: trimmedName,
+      number: newNumber,
     };
 
     const isDuplicate = persons.some(
@@ -22,16 +26,24 @@ const App = () => {
     } else {
       setPersons([...persons, newPerson]);
       setNewName("");
+      setNewNumber("");
     }
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addContact}>
         <div>
           name:{" "}
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -39,7 +51,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {`${person.name} ${person.number}`}
+        </p>
       ))}
     </div>
   );
