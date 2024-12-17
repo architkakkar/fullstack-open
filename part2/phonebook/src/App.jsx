15b2c70badd62592a372f0ea5021227a633d4c24
Archit Kakkar <education.archit@gmail.com>
2024-12-17 16:00:17 +0530

    exercise 2.6: create phonebook project; add name of a person to the phonebook.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,19 +4,33 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addName = (e) => {
+    e.preventDefault();
+
+    const newPerson = {
+      name: newName,
+    };
+
+    setPersons([...persons, newPerson]);
+    setNewName("");
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name:{" "}
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
 };
