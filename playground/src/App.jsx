0d261878d96618963c9b24abb2c27b40f396565c
Archit Kakkar <education.archit@gmail.com>
2024-12-22 20:30:41 +0530

    part 2d practice exercises completed; altering data in server.

--- playground/src/App.jsx
+++ playground/src/App.jsx
@@ -1,22 +1,18 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import noteService from "./services/notes";
 
 import Note from "./components/Note";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("a new note...");
+  const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    console.log("effect");
-
-    axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
-      setNotes(response.data);
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
     });
   }, []);
-  console.log("render", notes.length, "notes");
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
@@ -24,19 +20,33 @@ const App = () => {
     event.preventDefault();
 
     const noteObject = {
-      id: String(notes.length + 1),
       content: newNote,
       important: Math.random() < 0.5,
     };
 
-    setNotes(notes.concat(noteObject));
-    // setNotes([...notes, noteObject]); // same as above but using the spread operator which is more common
-    setNewNote("");
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value);
     setNewNote(event.target.value);
+  };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id === id ? returnedNote : note)));
+      })
+      .catch(() => {
+        alert(`the note '${note.content}' was already deleted from server`);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
   };
 
   return (
@@ -49,11 +59,20 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} required />
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+          required
+          placeholder="a new note..."
+        />
         <button type="submit">save</button>
       </form>
     </div>
--- playground/src/components/Note.jsx
+++ playground/src/components/Note.jsx
@@ -1,12 +1,19 @@
 import PropTypes from "prop-types";
 
-const Note = ({ note }) => {
-  // console.log(note);
-  return <li>{note.content}</li>;
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important ? "make not important" : "make important";
+
+  return (
+    <li>
+      {note.content}{" "}
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  );
 };
 
 Note.propTypes = {
   note: PropTypes.object.isRequired,
+  toggleImportance: PropTypes.func.isRequired,
 };
 
 export default Note;
