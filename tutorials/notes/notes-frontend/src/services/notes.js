2f809055a83d0ea5feaa4b9e9601149f611eb881
Archit Kakkar <education.archit@gmail.com>
2024-12-30 18:31:22 +0530

    update baseURL.

--- tutorials/notes/notes-frontend/src/services/notes.js
+++ tutorials/notes/notes-frontend/src/services/notes.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3000/notes";
+const baseUrl = "/api/notes";
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
+  const request = axios.get(baseUrl);
   const nonExisting = {
     id: 10000,
-    content: 'This note is not saved to server',
+    content: "This note is not saved to server",
     important: true,
-  }
-  return request.then(response => response.data.concat(nonExisting))
-}
+  };
+  return request.then((response) => response.data.concat(nonExisting));
+};
 
 const create = (newObject) => {
   const request = axios.post(baseUrl, newObject);
