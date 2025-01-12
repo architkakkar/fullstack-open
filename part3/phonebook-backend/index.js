1c4e1780ddc40295a1e0e38031cbff2a5cae68c5
Archit Kakkar <education.archit@gmail.com>
2025-01-12 16:35:28 +0530

    exercise 3.15: fix delete functionality to work with database.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -12,7 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 morgan.token("body", (request) => JSON.stringify(request.body));
-
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
@@ -30,10 +29,16 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/info", (request, response) => {
-  response.send(`
-    <p>Phonebook has info for ${contacts.length} people</p>
-    <p>${new Date()}</p>
-  `);
+  Person.countDocuments()
+    .then((noOfRecords) => {
+      response.send(`
+        <p>Phonebook has info for ${noOfRecords} people</p>
+        <p>${new Date()}</p>
+      `);
+    })
+    .catch((error) => {
+      response.status(500).send("Error retrieving document info");
+    });
 });
 
 app.get("/api/persons/:id", (request, response) => {
@@ -49,14 +54,15 @@ app.get("/api/persons/:id", (request, response) => {
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  const contact = contacts.find((contact) => contact.id === id);
 
-  if (contact) {
-    contacts = contacts.filter((contact) => contact.id !== id);
-    response.status(204).end();
-  } else {
-    response.status(404).send({ error: `No contact exists with id: ${id}` });
-  }
+  Person.findByIdAndDelete(id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => {
+      console.log(error.message);
+      response.status(400).send({ error: "invalid id." });
+    });
 });
 
 app.post("/api/persons", (request, response) => {
