1a0cc63abbd971cddb4e0c7078485f351b5a17d5
Archit Kakkar <education.archit@gmail.com>
2025-01-19 01:39:14 +0530

    exercise 4.1: create new node+express+mongodb project for blog app.

---
+++ part4/blog-backend/README.md
@@ -0,0 +1,1 @@
+Create a new `.env` file and provide your `MONGODB_URI` value.
---
+++ part4/blog-backend/index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const mongoUrl = process.env.MONGODB_URI;
+
+console.log("connecting to:", mongoUrl);
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("connected to mongodb.");
+  })
+  .catch((error) => console.error(error));
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
