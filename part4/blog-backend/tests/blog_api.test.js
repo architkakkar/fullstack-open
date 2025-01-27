28f4102fcb1ced6b211fff326d3c4e2f98cb31f4
Archit Kakkar <education.archit@gmail.com>
2025-01-27 20:00:15 +0530

    exercise 4.10: test that verifies that making an HTTP POST request to the /api/blogs URL successfully

--- part4/blog-backend/tests/blog_api.test.js
+++ part4/blog-backend/tests/blog_api.test.js
@@ -1,11 +1,22 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach, before } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const helper = require("./test_helper");
+const Blog = require("../models/blog");
 
 const app = require("../app");
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  for (let blog of helper.initialBlogs) {
+    const blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+});
 
 // use `npm test -- --test-name-pattern="all blogs are returned"` to execute single test.
 test("all blogs are returned", async () => {
@@ -14,7 +25,7 @@ test("all blogs are returned", async () => {
     .expect(200)
     .expect("Content-Type", /application\/json/);
 
-  assert.strictEqual(response.body.length, 2);
+  assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
 
 test("blogs have an id field", async () => {
@@ -26,6 +37,27 @@ test("blogs have an id field", async () => {
   const blogKeys = Object.keys(response.body[0]);
 
   assert(blogKeys.includes("id"));
+});
+
+test("a valid blog can be added", async () => {
+  const newBlog = {
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+
+  const titles = blogsAtEnd.map((b) => b.title);
+  assert(titles.includes(newBlog.title));
 });
 
 after(async () => {
