7bcbf99cbecb159bbc6b066e825bef182f7be4b6
Archit Kakkar <education.archit@gmail.com>
2025-01-27 18:18:15 +0530

    exercise 4.9: a test that verifies that the unique identifier property of the blog posts is named 'id'.

--- part4/blog-backend/tests/blog_api.test.js
+++ part4/blog-backend/tests/blog_api.test.js
@@ -7,6 +7,7 @@ const app = require("../app");
 
 const api = supertest(app);
 
+// use `npm test -- --test-name-pattern="all blogs are returned"` to execute single test.
 test("all blogs are returned", async () => {
   const response = await api
     .get("/api/blogs")
@@ -14,6 +15,17 @@ test("all blogs are returned", async () => {
     .expect("Content-Type", /application\/json/);
 
   assert.strictEqual(response.body.length, 2);
+});
+
+test("blogs have an id field", async () => {
+  const response = await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  const blogKeys = Object.keys(response.body[0]);
+
+  assert(blogKeys.includes("id"));
 });
 
 after(async () => {
