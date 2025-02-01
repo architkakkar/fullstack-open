68f465dc9cf39ef7af2aa73dab8bab4adeef902f
Archit Kakkar <education.archit@gmail.com>
2025-02-02 00:45:53 +0530

    part 4c 'creating users' practice exercise completed.

--- tutorials/notes/notes-backend/models/user.js
+++ tutorials/notes/notes-backend/models/user.js
@@ -1,13 +1,37 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
     unique: true, // this ensures the uniqueness of username
+    minlength: [3, "Username must be at least 3 characters long"],
+    validate: {
+      validator: function (v) {
+        return /^[a-zA-Z0-9]+$/.test(v); // only allows alphanumeric characters
+      },
+      message: (props) =>
+        `${props.value} is not a valid username! Only alphanumeric characters are allowed.`,
+    },
   },
   name: String,
-  passwordHash: String,
+  passwordHash: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return validator.isStrongPassword(v, {
+          minLength: 8,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1,
+        });
+      },
+      message: "Password is not strong enough",
+    },
+  },
   notes: [
     {
       type: mongoose.Schema.Types.ObjectId,
