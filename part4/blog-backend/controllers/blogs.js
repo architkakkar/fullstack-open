31a49cefad159f4ab99ed15ccba580952f38d82b
Archit Kakkar <education.archit@gmail.com>
2025-02-10 16:49:46 +0530

    exercise 4.19: Modify adding new blogs so that it is only possible if a valid token is sent with the HTTP POST request.

--- part4/blog-backend/controllers/blogs.js
+++ part4/blog-backend/controllers/blogs.js
@@ -1,6 +1,17 @@
+const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -11,24 +22,26 @@ blogsRouter.post("/", async (request, response) => {
   if (!request.body.title || !request.body.url) {
     return response.status(400).json({ error: "title and url are required" });
   }
-  const { title, author, url, likes = 0, userId } = request.body;
 
-  const user = await User.findById(userId);
-  if (!user) {
-    return response.status(401).json({ error: "user not found" });
+  const { title, author, url, likes = 0 } = request.body;
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
   }
+
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title,
     author,
     url,
     likes,
-    user: userId,
+    user: user._id,
   });
 
   const savedBlog = await blog.save();
   user.blogs = [...user.blogs, savedBlog.id];
-
   await user.save();
 
   response.status(201).json(savedBlog);
