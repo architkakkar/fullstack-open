6cbf0ddb183272ea69fa6556f419ae8292f534ec
Archit Kakkar <education.archit@gmail.com>
2025-05-18 00:16:23 +0530

    part 5a 'creating new note & saving token to local storage' practice exercise completed.

--- tutorials/notes/notes-frontend/src/App.jsx
+++ tutorials/notes/notes-frontend/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import LoginView from "./components/LoginView";
 import NoteView from "./components/NoteView";
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
 
+import noteService from "./services/notes";
+
 const App = () => {
   const [user, setUser] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedNoteAppUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      noteService.setToken(user.token);
+    }
+  }, []);
+
   const onLogin = (user, status) => {
     if (status === "success") {
       setUser(user);
+      noteService.setToken(user.token);
+      window.localStorage.setItem("loggedNoteAppUser", JSON.stringify(user));
     } else {
       setErrorMessage("Wrong credentials");
       setTimeout(() => {
@@ -20,9 +33,28 @@ const App = () => {
     }
   };
 
+  const onLogout = () => {
+    setUser(null);
+    noteService.setToken(null);
+    window.localStorage.removeItem("loggedNoteAppUser");
+  };
+
   return (
     <div>
-      <h1>Notes</h1>
+      <header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <h1>Notes</h1>
+        {user !== null && (
+          <button type="button" onClick={onLogout}>
+            logout
+          </button>
+        )}
+      </header>
       <Notification message={errorMessage} />
       {user === null ? (
         <LoginView onLogin={onLogin} />
